f4a02829a5e4a920f7d35d4d5122c455f740d3bc
djjiang <1561624238@qq.com>
2019-01-29 09:56:25 +0800

    修改首页

--- build/relate.conf.js
+++ build/relate.conf.js
@@ -4,18 +4,18 @@
 const glob = require('glob')
 // 相关配置
 const getEntry =() => {
-    var entry = {};
+    var entry = {}
     //读取src目录所有page入口
     glob.sync('./src/page/**/*.js')
         .forEach((name) => {
             var start = name.indexOf('src/') + 4,
-                end = name.length - 3;
-            var eArr = [];
+                end = name.length - 3
+            var eArr = []
             var n = name.slice(start, end);
-            n = n.slice(0, n.lastIndexOf('/')); //保存各个组件的入口 
-            n = n.split('/')[1];
-            eArr.push(name);
-            entry[n] = eArr;
+            n = n.slice(0, n.lastIndexOf('/')) //保存各个组件的入口 
+            n = n.split('/')[1]
+            eArr.push(name)
+            entry[n] = eArr
         });
     return entry
 };
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,11 +1,10 @@
 import '#/css/reset.css'
-import './index.css'
+import './index.scss'
 import "@babel/polyfill"
 import $ from 'zepto'
 import Header from '@/component/header/header.handlebars'
-
 $("#app").append(Header({
-    title: 'mso 编译模板'
+    title: 'mso 编译模板展示内容'
 }))
 $(".app-btn").on("click", () => {
     window.location.href = window.origin + '/page1.html'
